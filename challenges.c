3fd56d298caa8775b76a1010fa1cffde2d11001d
KareemSab278 <kareemsab278@gmail.com>
2025-10-13 18:28:39 +0100

    did a binary thingy idk im tired

--- challenges.c
+++ challenges.c
@@ -1,36 +1,70 @@
+// https://www.geeksforgeeks.org/c/c-exercises/
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
-void flipInts(); void factorial(int num);
+void flipInts();
+void factorial(int num);
 void isEvenOrOdd(int number);
 void largestNumber(int numberOfElems, int numbers[numberOfElems]);
+int convertBinaryToInt(char binary[]);
 
 void challenges()
 {
-    factorial(5);
+    printf("output from binary: %d", convertBinaryToInt("00010010"));
 }
 
-void factorial(int num){
+// ###
+
+int convertBinaryToInt(char binary[])
+{
+    int output = 0, i = 0, loopSize = strlen(binary);
+
+    for (int i = 0; i < loopSize; i++)
+    {
+        output += pow((binary[i] - '0'), 2) * i;
+    }
+    return output;
+}
+
+void factorial(int num)
+{
     printf("factorial of %d:", num);
     int output = 1;
-    while (num != 0){
+    while (num != 0)
+    {
         output *= num;
         num--;
     }
     printf(" %d", output);
 }
 
-void largestNumber(int numberOfElems, int numbers[numberOfElems]){
+void largestNumber(int numberOfElems, int numbers[numberOfElems])
+{
     // i could use recursion to return largest num but meh
     int output = 0;
     for (int i = 0; i < numberOfElems; i++)
     {
-        while (output < numbers[i]){ output = numbers[i]; }
+        while (output < numbers[i])
+        {
+            output = numbers[i];
+        }
     }
     printf("output: %d", output);
 }
 
-void isEvenOrOdd(int number) {if (number % 2 == 0) {printf("even");} else { printf("odd"); }}
+void isEvenOrOdd(int number)
+{
+    if (number % 2 == 0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
 
 void flipInts()
 {
--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
     challenges();
     return 0;
 }
